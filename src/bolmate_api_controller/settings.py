97e577ad63e237e05a64b96751dce44494a1d31a
Dave van Rijn <develop@davevanrijn.nl>
2026-08-26 14:25:37 +0200

    Fix incorrectly forbid extra config fields

--- src/bolmate_api_controller/settings.py
+++ src/bolmate_api_controller/settings.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='config.ini')
+    model_config = SettingsConfigDict(env_file='config.ini', extra='allow')
 
     # Encryption
     bolmate_encrypt_secret: str
